8b5bb68eaff497bcd45126bbb8dc7182c5edb8bc
Dan Todd <dantodd@live.co.uk>
2020-12-01 08:30:37 +0000

    created first arrays

---
+++ date.js
@@ -0,0 +1,15 @@
+/* This program has been created to test my knowledge of early level
+Javascript, as well as version control through git and navigation in
+the terminal. I also hope, in some small cases, it may solve a tricky
+problem now and again. Namely, what the heck to do on a date. While
+very simple in execution, apparently we all need to start somewhere.*/
+
+// First array contains all food places. Would objects give future customisation?
+const food = [];
+
+// Second array contains all activities.
+const activity = [];
+
+// Third array contains our 'night cap' post activity activity.
+const nightcap = [];
+
